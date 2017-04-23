bcc19f0c10b09ba92bf608ff88857d7bd8a53f9b
raichand <shivshive04@yahoo.com>
2017-04-23 23:49:50 +0530

    adding project to git

---
+++ src/main/java/Jenkinstest/App.java
@@ -0,0 +1,13 @@
+package Jenkinstest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Jenkinstest/Sample.java
@@ -0,0 +1,28 @@
+package Jenkinstest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	@Test
+	public void Test_1()
+	{
+		WebDriver driver,driver2;
+		
+		System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		
+		System.setProperty("webdriver.gecko.driver", "./geckodriver.exe");
+		driver2 = new FirefoxDriver();
+		driver2.manage().window().maximize();
+		driver2.get("https://www.youtube.com");
+		
+	}
+
+}
