d1176ecd343c9fb0b23968a17d54111392ce458a
raichand <shivshive04@yahoo.com>
2017-04-29 18:28:18 +0530

    committed pom.xml with testng

--- src/test/java/Jenkinstest/Sample.java
+++ src/test/java/Jenkinstest/Sample.java
@@ -1,5 +1,6 @@
 package Jenkinstest;
 
+import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
